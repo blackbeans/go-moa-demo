3c874745de63338f75ab5eae2429c184ec372e23
blackbeans <blackbeans.zc@gmail.com>
2016-03-10 15:58:52 +0800

    modified:   build.sh 	modified:   go_moa_client.go

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 go get github.com/naoina/toml
 go get -insecure git.wemomo.com/bibi/go-moa/core
+go get -insecure git.wemomo.com/bibi/go-moa-client/client
 
 
-go build -o demo-server ./s
+go build -o demo-server go_moa_demo.go
 
-go build -o demo-client ./c 
+go build -o demo-client go_moa_client.go
 
-tar -zcvf go-moa-demo.tar.gz demo-server ./s/*.toml ./c/*.toml
+tar -zcvf go-moa-demo.tar.gz demo-server *.toml *.xml
 
 
--- go_moa_client.go
+++ go_moa_client.go
@@ -15,18 +15,28 @@ type CGoMoaDemo struct {
 	GetDemoName func(serviceUri, proto string) (CDemoResult, error)
 }
 
+type Session struct {
+	GetSessionByUid func(uid string) (string, error)
+}
+
 func main() {
 	consumer := client.NewMoaConsumer("go_moa_client.toml",
 		[]proxy.Service{proxy.Service{
-			ServiceUri: "/service/bibi/go-moa",
-			Instance:   &CGoMoaDemo{}},
+			ServiceUri: "/service/bibi-session",
+			Interface:  &Session{}},
 		})
-	h := consumer.GetService("/service/bibi/go-moa").(*CGoMoaDemo)
 
-	for i := 0; i < 10000; i++ {
-		a, err := h.GetDemoName("/service/user-profile", "redis")
+	h := consumer.GetService("/service/bibi-session").(*Session)
+	a, err := h.GetSessionByUid("1543")
 
-		fmt.Printf("GetDemoName|%s|%v\n", a, err)
-	}
+	fmt.Printf("GetSessionByUid|%s|%v\n", a, err)
+
+	// h := consumer.GetService("/service/bibi/go-moa").(*CGoMoaDemo)
+
+	// for i := 0; i < 10000; i++ {
+	// 	a, err := h.GetDemoName("/service/user-profile", "redis")
+
+	// 	fmt.Printf("GetDemoName|%s|%v\n", a, err)
+	// }
 
 }
