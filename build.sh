54c158b2f22a4e27057152a52a408599369e0bb4
blackbeans <blackbeans.zc@gmail.com>
2016-03-10 16:00:53 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ go build -o demo-server go_moa_demo.go
 
 go build -o demo-client go_moa_client.go
 
-tar -zcvf go-moa-demo.tar.gz demo-server *.toml *.xml
+tar -zcvf go-moa-demo.tar.gz demo-server demo-client *.toml *.xml
 
 
